2225ce2be7f6b1306f7eb8451fedd50241f1913d
kiserd <59572911+kiserd@users.noreply.github.com>
2020-08-26 08:50:10 -0700

    started problem setup and transitioned to easier problems second attempt

--- LC_4_MedianTwoArrays_00.py
+++ LC_4_MedianTwoArrays_00.py
@@ -3,4 +3,13 @@
 # Description: Initial attempt at LeetCode problem 4
 
 def findMedianSortedArrays(nums1: List[int], nums2: List[int]) -> float:
+    # create a couple helper vars to make code more readable
+    len1 = len(nums1)
+    len2 = len(nums2)
+    len_total = len1 + len2
+    nums1_beg = nums1[0]
+    nums1_end = nums1[len1 - 1]
+    nums2_beg = nums2[0]
+    nums2_end = nums2[len2 - 1]
     
+
